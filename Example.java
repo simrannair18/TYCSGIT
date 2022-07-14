f6916b26aaa33dffa73b75e122682bd75d662db3
simrannair18 <simrannair318@gmail.com>
2022-07-14 11:12:02 +0530

    java files

---
+++ Example.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class sum
+{
+public static void main()
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the value for n");
+int n=sc.nextInt();
+int sum=0;
+for(int i=1;i<=n;i++)
+sum=sum+i;
+System.out.println(sum);
+}
